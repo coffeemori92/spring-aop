9a40c45e31a2da94b0619185f78a56960004c5b9
coffeemori <coffeemori92@naver.com>
2022-05-14 16:12:46 +0900

    프록시 패턴

---
+++ src/main/java/hello/advanced/proxyconfig/AppV1Config.java
@@ -0,0 +1,24 @@
+package hello.advanced.proxyconfig;
+
+import hello.advanced.proxyapp.v1.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppV1Config {
+
+    @Bean("orderControllerProxyV1")
+    public OrderControllerV1 orderControllerV1() {
+        return new OrderControllerV1Impl(orderServiceV1());
+    }
+
+    @Bean("orderServiceProxyV1")
+    public OrderServiceV1 orderServiceV1() {
+        return new OrderServiceV1Impl(orderRepositoryV1());
+    }
+
+    @Bean("OrderRepositoryProxyV1")
+    public OrderRepositoryV1 orderRepositoryV1() {
+        return new OrderRepositoryV1Impl();
+    }
+}
---
+++ src/main/java/hello/advanced/proxyconfig/AppV2Config.java
@@ -0,0 +1,26 @@
+package hello.advanced.proxyconfig;
+
+import hello.advanced.proxyapp.v2.OrderControllerV2;
+import hello.advanced.proxyapp.v2.OrderRepositoryV2;
+import hello.advanced.proxyapp.v2.OrderServiceV2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppV2Config {
+
+    @Bean("orderControllerProxyV2")
+    public OrderControllerV2 orderControllerV2() {
+        return new OrderControllerV2(orderServiceV2());
+    }
+
+    @Bean("orderServiceProxyV2")
+    public OrderServiceV2 orderServiceV2() {
+        return new OrderServiceV2(orderRepositoryV2());
+    }
+
+    @Bean("OrderRepositoryProxyV2")
+    public OrderRepositoryV2 orderRepositoryV2() {
+        return new OrderRepositoryV2();
+    }
+}
