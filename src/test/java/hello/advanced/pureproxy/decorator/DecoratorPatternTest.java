17bb1b3b5e55019da57a4f459c71068593c74520
coffeemori <coffeemori92@naver.com>
2022-05-14 19:06:37 +0900

    데코레이터 패턴

---
+++ src/test/java/hello/advanced/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,36 @@
+package hello.advanced.pureproxy.decorator;
+
+import hello.advanced.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.advanced.pureproxy.decorator.code.MessageDecorator;
+import hello.advanced.pureproxy.decorator.code.RealComponent;
+import hello.advanced.pureproxy.decorator.code.TimeDecorator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        RealComponent realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
